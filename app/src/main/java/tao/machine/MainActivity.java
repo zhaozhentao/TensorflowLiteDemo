8bde4d8cb941ede80268b599234835c2c59e4703
zhaozhentao <qq344696734@gmail.com>
2022-05-20 15:00:50 +0800

    new

--- app/src/main/java/tao/machine/MainActivity.java
+++ app/src/main/java/tao/machine/MainActivity.java
@@ -1,14 +1,18 @@
 package tao.machine;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DigitClassifier digitClassifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.digitClassifier = new DigitClassifier(this);
     }
 }

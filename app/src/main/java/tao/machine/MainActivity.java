8afa1ed161a651003c2551d402fc646027cf32b8
zhaozhentao <qq344696734@gmail.com>
2022-05-20 17:01:11 +0800

    可以抠图

--- app/src/main/java/tao/machine/MainActivity.java
+++ app/src/main/java/tao/machine/MainActivity.java
@@ -1,14 +1,31 @@
 package tao.machine;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private DigitClassifier digitClassifier;
+
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +39,61 @@ public class MainActivity extends AppCompatActivity {
                     ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
                 });
         });
+
+        findViewById(R.id.take).setOnClickListener(view -> takePhoto());
+    }
+
+    private File createImageFile() {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile = null;
+        try {
+            imageFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imageFile;
+    }
+
+    public void takePhoto() {
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = createImageFile();
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        imageUri = FileProvider.getUriForFile(this, "tao.machine.provider", file);
+        it.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(it, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Toast.makeText(this, "回调了", Toast.LENGTH_SHORT).show();
+
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        try {
+            Bitmap photo = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            photo = Bitmap.createBitmap(photo, 0, 0, photo.getWidth(), photo.getHeight(), matrix, true);
+            digitClassifier.classifyAsync(photo)
+                .addOnSuccessListener(bitmap -> {
+                    Toast.makeText(this, "识别成功", Toast.LENGTH_SHORT).show();
+                    ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
+                });
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }

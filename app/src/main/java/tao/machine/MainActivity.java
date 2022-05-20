4a69c734959f7ca5ff249a9f08d61ca3107cd98d
zhaozhentao <qq344696734@gmail.com>
2022-05-20 15:55:03 +0800

    可以找到车牌

--- app/src/main/java/tao/machine/MainActivity.java
+++ app/src/main/java/tao/machine/MainActivity.java
@@ -1,14 +1,10 @@
 package tao.machine;
 
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
         this.digitClassifier = new DigitClassifier(this);
         this.digitClassifier.initialize().addOnSuccessListener(unused -> {
-            Log.e("init", "finish");
-
             digitClassifier.classifyAsync(BitmapFactory.decodeResource(getResources(), R.mipmap.tesla))
                 .addOnSuccessListener(bitmap -> {
                     ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
